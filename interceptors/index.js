7b9e9ce73f6b3950e66457626cb3aef6ad4e448a
Roshan Gade <roshangade@gmail.com>
2018-08-11 21:58:57 +0530

    Added interceptor in router

--- interceptors/index.js
+++ interceptors/index.js
@@ -3,3 +3,16 @@
  * Copyright(c) 2018 Roshan Gade
  * MIT Licensed
  */
+const request = require('./request');
+const response = require('./response');
+const urlParser = require('./url-parser');
+
+const interceptors = () => {
+    return Object.assign({
+        request,
+        response,
+        urlParser
+    });
+};
+
+module.exports = interceptors();
