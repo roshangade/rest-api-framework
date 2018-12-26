efb6fd95b15fda0d039e9fa29d680d6b255c15a4
Roshan Gade <roshangade@gmail.com>
2018-12-26 23:49:09 +0530

    Fixed function scopes

--- utils/url.js
+++ utils/url.js
@@ -12,69 +12,64 @@ const query = require('querystring');
  */
 const Url = function () {
     return Object.freeze({
-        parseUrl,
-        parseQuery,
-        parseParams,
-        compile
-    });
-}
+        /**
+         * Parse URL and returns object
+         */
+        parseUrl(...args) {
+            return url.parse.apply(null, args);
+        },
 
-/**
- * Parse URL and returns object
- */
-const parseUrl = function (...args) {
-    return url.parse.apply(null, args);
-};
+        /**
+         * Parse querystring into object
+         */
+        parseQuery(...args) {
+            return query.parse.apply(null, args);
+        },
 
-/**
- * Parse querystring into object
- */
-const parseQuery = function (...args) {
-    return query.parse.apply(null, args);
-};
+        /**
+         * Parse URL and get request params
+         */
+        parseParams(path, keys, pattern) {
+            let params = {};
+            let matches = pattern.exec(path);
+            matches.shift();
+            keys.forEach((key, i) => {
+                if (matches[i]) params[key] = matches[i] || '';
+            });
+            return params;
+        },
 
-/**
- * Parse URL and get request params
- */
-const parseParams = function (path, keys, pattern) {
-    let params = {};
-    let matches = pattern.exec(path);
-    matches.shift();
-    keys.forEach((key, i) => {
-        if (matches[i]) params[key] = matches[i] || '';
-    });
-    return params;
-};
+        /**
+         * Compile router URL and create RegExp for easy use
+         */
+        compile(route) {
+            let keys = [];
+            let pattern = '';
+            let sep = '/';
 
-/**
- * Compile router URL and create RegExp for easy use
- */
-const compile = function (route) {
-    let keys = [];
-    let pattern = '';
-    let sep = '/';
+            route.split(sep).filter((val) => !!val).forEach((val) => {
+                switch (val.charCodeAt(0)) {
+                    case 42:
+                        pattern += '(.*)';
+                        break;
+                    case 58:
+                        let length = val.length - 1;
+                        let optional = val.charCodeAt(length) === 63;
+                        keys.push(val.substring(1, optional ? length : undefined));
+                        pattern += optional ? '(?:/([^/]+?))?' : sep + '([^/]+?)';
+                        break;
+                    default:
+                        pattern += sep + val;
+                }
+            });
 
-    route.split(sep).filter((val) => !!val).forEach((val) => {
-        switch (val.charCodeAt(0)) {
-            case 42:
-                pattern += '(.*)';
-                break;
-            case 58:
-                let length = val.length - 1;
-                let optional = val.charCodeAt(length) === 63;
-                keys.push(val.substring(1, optional ? length : undefined));
-                pattern += optional ? '(?:/([^/]+?))?' : sep + '([^/]+?)';
-                break;
-            default:
-                pattern += sep + val;
+            (!!keys.length) && (pattern += '(?:/)?');
+            pattern = new RegExp('^' + pattern + '\/?$', 'i');
+            let data = { pattern };
+            (!!keys.length) && (data.keys = keys);
+            return data;
         }
     });
-
-    (!!keys.length) && (pattern += '(?:/)?');
-    pattern = new RegExp('^' + pattern + '\/?$', 'i');
-    let data = { pattern };
-    (!!keys.length) && (data.keys = keys);
-    return data;
 };
 
 module.exports = Url();
